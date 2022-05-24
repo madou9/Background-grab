592eed528c4355f2c7bfc92f2420d43e891e4992
MAD-G <codeloccol@iMac-de-Codeloccol-60.local>
2022-05-24 22:36:54 +0100

    first creation

---
+++ script.js
@@ -0,0 +1,19 @@
+const css = document.querySelector('h3');
+const color1 = document.querySelector('.color1');
+const color2 = document.querySelector('.color2');
+const body = document.querySelector('#body');
+
+
+color1.addEventListener('input', function(){
+  body.style.background =
+ `linear-gradient(to right,${color1.value},${color2.value})`;
+ css.textContent = body.style.background ;
+});
+color2.addEventListener('input', function(){
+  body.style.background =
+ `linear-gradient(to right,${color1.value},${color2.value})`;
+ css.textContent = body.style.background ;
+});
+ 
+
+
